2471ad0acf8f17f85c3cc49a491156be46548a9e
Toni Čizmić <tcizmic27@gmail.com>
2025-04-29 09:18:54 +0200

    CORS policies

--- src/main/java/hr/tvz/tim2/webserver/security/WebConfig.java
+++ src/main/java/hr/tvz/tim2/webserver/security/WebConfig.java
@@ -9,8 +9,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*");
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "PATCH", "TRACE", "CONNECT", "PROPFIND", "PROPPATCH")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
--- src/main/java/hr/tvz/tim2/webserver/stock/StockController.java
+++ src/main/java/hr/tvz/tim2/webserver/stock/StockController.java
@@ -7,7 +7,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
